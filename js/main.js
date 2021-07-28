d809bcc4629006f7470e961aedabb91014fb28c5
Anastasiya <pustoe0@gmail.com>
2021-07-28 11:54:09 +0300

    fixed html

--- js/main.js
+++ js/main.js
@@ -3,3 +3,20 @@ import cardOptions from "./cardOptions.js";
 
 dragAndDrop();
 cardOptions();
+
+let array = [2, 4, 6];
+const count = 10
+let i = 2;
+
+function fibonachi() {
+    array.push((array[i-1] + array[i]));
+    i++
+    if (array.length < count) {
+            fibonachi()
+    } else {
+        console.log(array);
+    }
+    
+}
+
+fibonachi();    
