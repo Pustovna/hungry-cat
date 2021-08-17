e7cfc1f4033d574e13a1d90f75ed33fdb6cc3bfa
Anastasiya <pustoe0@gmail.com>
2021-08-17 21:30:53 +0300

    Added 100% event in reft

--- js/refr.js
+++ js/refr.js
@@ -11,16 +11,27 @@ export default class Refrigerator {
     }
 
     progress() {
-        let ind1Width = (+this.totalCount.satiety * 100 / this.total);
-        let ind2Width = (+this.totalCount.mess * 100 / this.total);
-        
-        this.ind1.style.width = ind1Width + '%';
-        this.ind2.style.width = ind2Width + '%';
+        let ind1Width = Math.round((+this.totalCount.satiety * 100 / this.total));
+        let ind2Width = Math.round((+this.totalCount.mess * 100 / this.total));
 
-        this.ind1.closest('.stat').children[0].children[1].textContent = Math.round(ind1Width) + '%';
-        this.ind2.closest('.stat').children[0].children[1].textContent = Math.round(ind2Width) + '%';
+        if (ind1Width > 100) {
+            this.ind1.style.width = 100 + '%';
+            this.ind1.closest('.stat').children[0].children[1].textContent = 'Кот переполнен!';
+        } else {
+            this.ind1.style.width = ind1Width + '%';
+            this.ind1.closest('.stat').children[0].children[1].textContent = ind1Width + '%';
+        }
+
+        if (ind2Width > 100){
+            this.ind2.style.width = 100 + '%';
+            this.ind2.closest('.stat').children[0].children[1].textContent = 'Кажется, это конец...';
+        } else {
+            this.ind2.style.width = ind2Width + '%';
+            this.ind2.closest('.stat').children[0].children[1].textContent = ind2Width + '%';
+        }
         
-        console.log(this.totalCount.satiety + ' ' + this.totalCount.mess);
+        
+        console.log(ind1Width, ind2Width);
     }
     
 
