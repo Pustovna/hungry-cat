06bae207ddec7e5ecc3b5e575289cb75a4b511d3
Anastasiya <pustoe0@gmail.com>
2021-09-19 22:25:06 +0300

    added footer and tooltip

--- js/main.js
+++ js/main.js
@@ -8,3 +8,15 @@ cardOptions();
 filters(".menu__item", ".cards__container");
 filters(".filter__item", ".card");
 sort();
+
+
+const arrow = document.querySelector('.arrow__wrap');
+
+
+if (arrow) { 
+    const body = document.querySelector('body');
+    body.addEventListener('click', () => {
+        console.log('dfdf')
+        arrow.remove();
+    }, {once: true})
+}
